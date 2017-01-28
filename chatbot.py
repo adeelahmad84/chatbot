c84c59981cbbe3ca01cdb641fcfbace94991f8c4
Adeel Ahmad <adeelahmad84@me.com>
2017-01-28 03:00:02 +0000

    First commit

---
+++ chatbot.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+
+"""
+File:           chatbot.py
+Author:         Adeel Ahmad
+Email:          adeelahmad84@me.com
+Github:         https://github.com/adeelahmad84
+Description:    My first chatbot.
+"""
+
+from chatterbot import ChatBot
+from settings import TWITTER
+import logging
+import unittest
+
+def main():
+	logging.basicConfig(level=logging.INFO)
+
+	chatbot = ChatBot(
+		'Adeel',
+		#storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
+		input_adapter="chatterbot.input.TerminalAdapter",
+		output_adapter="chatterbot.output.TerminalAdapter",
+		logic_adapters=[
+			"chatterbot.logic.MathematicalEvaluation",
+			"chatterbot.logic.TimeLogicAdapter",
+			"chatterbot.logic.BestMatch"
+		],
+		database="./twitter-database.db",
+		twitter_consumer_key=TWITTER["CONSUMER_KEY"],
+		twitter_consumer_secret=TWITTER["CONSUMER_SECRET"],
+		twitter_access_token_key=TWITTER["ACCESS_TOKEN"],
+		twitter_access_token_secret=TWITTER["ACCESS_TOKEN_SECRET"],
+		trainer="chatterbot.trainers.TwitterTrainer"
+	)
+
+	chatbot.train()
+
+	chatbot.logger.info('Trained database generated successfully!')
+
+	while True:
+	    try:
+	     bot_input = chatbot.get_response(None)
+
+	    except(KeyboardInterrupt, EOFError, SystemExit):
+		break
+
+if __name__ == '__main__':
+        import doctest
+        doctest.testmod()
+        main()
---
+++ settings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+
+"""
+File:           settings.py
+Author:         Adeel Ahmad
+Email:          adeelahmad84@me.com
+Github:         https://github.com/adeelahmad84
+Description:    Twitter API settings script.
+"""
+
+import unittest
+
+TWITTER = {
+    "CONSUMER_KEY": "pLm9eKGAVqNY3f4x9SV3Ex50O",
+    "CONSUMER_SECRET": "cl1yivmDsj4S7DY92VF7HNO4PHL8QdB3q8JAoZJhfxdQzE69Sq",
+    "ACCESS_TOKEN": "21188640-kTvhs9bpEDD7yxUyhkatXag7LACs2ql1xHDvyPwiX",
+    "ACCESS_TOKEN_SECRET": "qCtFqqxUmnIIcBLQyaFPvf2RvvNcUB6BXU8ScnDCMoqHQ"
+}
