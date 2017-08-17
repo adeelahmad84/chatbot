b5438d5db8cf7930c3d2cdd38231a0b26a7fefd7
Adeel Ahmad <adeelahmad786@gmail.com>
2017-08-17 12:22:41 +0100

    removed sensitive data

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ Description:    Twitter API settings script.
 #import unittest
 
 TWITTER = {
-    "CONSUMER_KEY": "pLm9eKGAVqNY3f4x9SV3Ex50O",
-    "CONSUMER_SECRET": "cl1yivmDsj4S7DY92VF7HNO4PHL8QdB3q8JAoZJhfxdQzE69Sq",
-    "ACCESS_TOKEN": "21188640-kTvhs9bpEDD7yxUyhkatXag7LACs2ql1xHDvyPwiX",
-    "ACCESS_TOKEN_SECRET": "qCtFqqxUmnIIcBLQyaFPvf2RvvNcUB6BXU8ScnDCMoqHQ"
+    "CONSUMER_KEY": "blahblahblah",
+    "CONSUMER_SECRET": "blahblahblah",
+    "ACCESS_TOKEN": "21188640-blahblahblah",
+    "ACCESS_TOKEN_SECRET": "blahblahblah"
 }
